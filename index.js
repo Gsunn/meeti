b882a433463f90c10700ec8ed61241341f7b6302
gsun75 <jesus.delvalvelasco@gmail.com>
2021-06-28 20:27:14 +0200

    01

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const path = require('path')
+const router = require('./routes')
+const expressLayouts = require('express-ejs-layouts')
 require('dotenv').config({path: 'variables.env'})
 
 const app = express()
 
+app.use(expressLayouts)
+// View engine EJS
+app.set('view engine', 'ejs')
+
+app.set('views', path.join(__dirname, './views'))
+app.use(express.static('public'))
+
+//Routing
+app.use('/', router())
 
 
 app.listen(process.env.PORT, ()=>{
