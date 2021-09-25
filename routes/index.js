b91e9af66ce07cc43ab2cbe01f564c7c585c2a54
gsun75 <jesus.delvalvelasco@gmail.com>
2021-09-25 09:21:37 +0200

    239

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,84 @@
 const express = require('express')
 const router = express.Router()
 
-module.exports = function (){
-    router.get('/', (req, res) => {
-        res.render('home')
-    })
+const homeController = require('../controllers/homeCotroller')
+const usuariosController = require('../controllers/usuariosController')
+const authController = require('../controllers/authController')
+
+const adminController = require('../controllers/adminController')
+const gruposController = require('../controllers/gruposController')
+const meetiController = require('../controllers/meetiController')
+
+module.exports = () => {
+    router.get('/', homeController.home)
 
 
-    router.get('/crear-cuenta', (req, res) =>{
-        res.render('crear-cuenta')
-    })
+    router.get('/crear-cuenta', usuariosController.formCrearCuenta)
+    router.post('/crear-cuenta', usuariosController.crearNuevaCuenta)
+    router.get('/confirmar-cuenta/:correo', usuariosController.confirmarCuenta);
+
+
+    router.get('/iniciar-sesion', usuariosController.formIniciarSesion)
+    router.post('/iniciar-sesion', authController.autenticarUsuario)
+
+
+    // Administracion
+    router.get('/administracion', 
+        authController.usuarioAutenticado,
+        adminController.panelAdministracion)
+
+    
+    router.get('/nuevo-grupo', 
+        authController.usuarioAutenticado,
+        gruposController.formNuevoGrupo)
+
+    router.post('/nuevo-grupo', 
+        authController.usuarioAutenticado,
+        gruposController.subirImagen,
+        gruposController.crearGrupo)
+
+    router.get('/editar-grupo/:grupoId', 
+        authController.usuarioAutenticado,
+        gruposController.formEditarGrupo)
+
+    router.post('/editar-grupo/:grupoId', 
+        authController.usuarioAutenticado,
+        gruposController.editarGrupo)
+
+    // Editar imagen de grupo
+    router.get('/imagen-grupo/:grupoId', 
+        authController.usuarioAutenticado,
+        gruposController.formEditarImagen)
+
+    
+    router.post('/imagen-grupo/:grupoId', 
+        authController.usuarioAutenticado,
+        gruposController.subirImagen,
+        gruposController.editarImagen)
+
+    
+    // Eliminar grupos
+    router.get('/eliminar-grupo/:grupoId',
+        authController.usuarioAutenticado,
+        gruposController.formEliminarGrupo) 
+
+    router.post('/eliminar-grupo/:grupoId',
+    authController.usuarioAutenticado,
+    gruposController.eliminarGrupo) 
+
+    // Meetis
+    router.get('/nuevo-meeti',
+        authController.usuarioAutenticado,
+        meetiController.formNuevoMeeti) 
+
+
+    router.post('/nuevo-meeti',
+        authController.usuarioAutenticado,
+        meetiController.sanitizarMeeti,
+        meetiController.crearMeeti) 
+
+    // Editar Meeti
+    
 
     return router
 }
